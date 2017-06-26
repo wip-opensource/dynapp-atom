01bf9dbb88c7630cb87ef066975d97db536b2fc7
unknown <rahlenoskar@gmail.com>
2017-06-26 10:51:20 +0200

    Adding delete funcitonality and some bugfixes

---
+++ spec/dynapp-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DynappAtomView from '../lib/dynapp-atom-view';
+
+describe('DynappAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
